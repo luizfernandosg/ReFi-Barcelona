8dcf3b823e7f70d93bd49cec06ec01cd6b7d7f81
Luiz Fernando <luizfernandolfsg@gmail.com>
2023-11-30 22:58:57 -0300

    231130

---
+++ publish.js
@@ -0,0 +1,21 @@
+{
+    "div": {
+      "class": "top-bar-links",
+      "content": [
+        {
+          "a": {
+            "class": "top-bar-link",
+            "href": "https://publish.obsidian.md/refi-barcelona/ReFi+Barcelona",
+            "text": "About ReFi BCN"
+          }
+        },
+        {
+          "a": {
+            "class": "top-bar-link",
+            "href": "https://publish.obsidian.md/refi-barcelona/Ecosystem+mapping",
+            "text": "Ecosystem mapping"
+          }
+        },
+      ]
+    }
+  }
